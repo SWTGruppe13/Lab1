bd9c385126679ffa36eb36a0874e67e5c6eb224a
FranzVDB <Franz96ftw@hotmail.com>
2019-08-29 13:13:13 +0200

    1st push

---
+++ Calculator/CalculatorTest/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabExercise_Hand_testing_class_Calculator;
+using NUnit.Framework;
+
+namespace CalculatorTest
+{
+    public class Class1
+    {
+	    private Calculator uut;
+
+	    [SetUp]
+		public void Setup()
+	    {
+			uut = new Calculator();
+	    }
+
+		[Test]
+		public void AddTwoNumbers_Four_Plus_Two_ReturnSix()
+		{
+			Assert.That(uut.Add(4,2),Is.EqualTo(6));
+		}
+
+		[Test]
+		public void MultiplyTwoNumbers_Four_Times_Two_ReturnEigth()
+		{
+			Assert.That(uut.Multiply(4, 2), Is.EqualTo(8));
+		}
+
+		[Test]
+		public void SubtractTwoNumbers_Four_Minus_Two_ReturnTwo()
+		{
+			Assert.That(uut.Subtract(4, 2), Is.EqualTo(2));
+		}
+
+		[Test]
+		public void PowerOfTwoNumbers_Four_ToThePowerOf_Two_Return16()
+		{
+			Assert.That(uut.Power(4, 2), Is.EqualTo(16));
+		}
+	}
+}
---
+++ Calculator/LabExercise_Hand-testing_class_Calculator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExercise_Hand_testing_class_Calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Calculator cal = new Calculator();
+
+			Console.WriteLine($"2 + 2 er {cal.Add(2,2)}");
+			Console.WriteLine($"5 - 2 er {cal.Subtract(5, 2)}");
+			Console.WriteLine($"4 * 2 er {cal.Multiply(4, 2)}");
+			Console.WriteLine($"2^4 er {cal.Power(2, 4)}");
+
+			if (cal.Add(2, 2) == 4)
+			{
+				Console.WriteLine("2 + 2 er 4, derved virker Add");
+			}
+			else
+			{
+				Console.WriteLine($"Add virker ikke, og gav værdien: {cal.Add(2,2)}");
+			}
+
+			if (cal.Subtract(5, 2) == 3)
+			{
+				Console.WriteLine("5 - 2 er 3, derved virker Subtract");
+			}
+			else
+			{
+				Console.WriteLine($"Subtract virker ikke, og gav værdien: {cal.Subtract(5, 2)}");
+			}
+
+			if (cal.Multiply(4, 2) == 8)
+			{
+				Console.WriteLine("4 * 2 er 8, derved virker Multiply");
+			}
+			else
+			{
+				Console.WriteLine($"Multiply virker ikke, og gav værdien: {cal.Multiply(4, 2)}");
+			}
+
+			if (cal.Power(2, 4) == 16)
+			{
+				Console.WriteLine(" 2^4 er 16, derved virker Power");
+			}
+			else
+			{
+				Console.WriteLine($"Power virker ikke, og gav værdien: {cal.Power(2, 4)}");
+			}
+
+		}
+	}
+}
