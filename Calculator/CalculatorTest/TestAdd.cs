6a9d6300681e0c64a6fbc6329e9c1a2b5a1d4682
Emil Knudsen <eknudsen1992@hotmail.com>
2019-09-05 13:30:40 +0200

    TestSubstrac added

--- Calculator/CalculatorTest/TestAdd.cs
+++ Calculator/CalculatorTest/TestAdd.cs
@@ -8,7 +8,7 @@ using NUnit.Framework;
 
 namespace CalculatorTest
 {
-    public class Class1
+    public class ClTestAddass1
     {
         private Calculator uut;
 
--- Calculator/CalculatorTest/TestSubtract.cs
+++ Calculator/CalculatorTest/TestSubtract.cs
@@ -8,7 +8,7 @@ using NUnit.Framework;
 
 namespace CalculatorTest
 {
-    public class Class1
+    public class TestSubstract
     {
         private Calculator uut;
 
@@ -18,14 +18,38 @@ namespace CalculatorTest
             uut = new Calculator();
         }
 
-        [Test]
-        public void SubtractTwoNumbers_Four_Minus_Two_ReturnTwo()
+        [TestCase(5,5,0)]
+        [TestCase(-2,-2,0)]
+        [TestCase(5.5,5.5,0)]
+        public void Subtract_Equals_Zero(double a, double b, double c)
         {
-            Assert.That(uut.Subtract(4, 2), Is.EqualTo(2));
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(c));
         }
 
+        [TestCase(0, 5, -5)]
+        [TestCase(2, 3, -1)]
+        [TestCase(5, 5.5, -0.5)]
+        public void Subtract_Equals_Negative_Result(double a, double b, double c)
+        {
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(c));
+        }
 
+        [TestCase(0, -5, 5)]
+        [TestCase(2, -3, 5)]
+        [TestCase(-2, -2, 0)]
+        [TestCase(5, -5.5, 10.5)]
+        public void Subtract_Negative_Number_Equals_Addition(double a, double b, double c)
+        {
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(c));
+        }
 
+        [TestCase(-2, -5, 3)]
+        [TestCase(-3, -3, 0)]
+        [TestCase(-5, -1, -4)]
+        [TestCase(-5, -5.5, 0.5)]
+        public void Subtract_Two_Negative_Numbers(double a, double b, double c)
+        {
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(c));
+        }
     }
-
 }
